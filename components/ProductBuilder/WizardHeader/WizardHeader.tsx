ae97ffcad7ccdb9c04342785ab4377e609bdc7f4
Viktor <ricchiuto.viktor@gmail.com>
2021-07-27 23:13:04 +0200

    fix: make header title dynamic

--- components/ProductBuilder/WizardHeader/WizardHeader.tsx
+++ components/ProductBuilder/WizardHeader/WizardHeader.tsx
@@ -5,18 +5,22 @@ interface Props {
   selectStep(step: number): void;
 }
 
+const headerTitles = ["select model", "select color", "accessories", "summary"];
+
 function WizardHeader({ step, selectStep }: Props): JSX.Element {
   return (
     <div className={styles.container}>
-      <h2>Select Model</h2>
-      <div className={styles.stepCounter}>step {step + 1} of 4</div>
+      <h2>{headerTitles[step]}</h2>
+      <div className={styles.stepCounter}>Step {step + 1} of 4</div>
       <ul className={styles.links}>
         <li onClick={() => selectStep(0)}>models</li>
         <li onClick={() => selectStep(1)}>colors</li>
         <li onClick={() => selectStep(2)}>accessories</li>
         <li onClick={() => selectStep(3)}>summary</li>
       </ul>
-      <button className={styles.backButton}>Article & Download</button>
+      {step === 0 && (
+        <button className={styles.backButton}>Article & Download</button>
+      )}
     </div>
   );
 }
