bdec788f6cc8ad7916ccd4e738233de6fcfb9de0
Viktor <ricchiuto.viktor@gmail.com>
2021-07-27 22:13:02 +0200

    feat: add basic layout to wizard header

--- components/ProductBuilder/WizardHeader/WizardHeader.tsx
+++ components/ProductBuilder/WizardHeader/WizardHeader.tsx
@@ -7,15 +7,16 @@ interface Props {
 
 function WizardHeader({ step, selectStep }: Props): JSX.Element {
   return (
-    <div>
-      <div>wizard header</div>
-      <div>step {step + 1} of 4</div>
-      <ul>
+    <div className={styles.container}>
+      <h2>Select Model</h2>
+      <div className={styles.stepCounter}>step {step + 1} of 4</div>
+      <ul className={styles.links}>
         <li onClick={() => selectStep(0)}>models</li>
         <li onClick={() => selectStep(1)}>colors</li>
         <li onClick={() => selectStep(2)}>accessories</li>
         <li onClick={() => selectStep(3)}>summary</li>
       </ul>
+      <button className={styles.backButton}>Article & Download</button>
     </div>
   );
 }
