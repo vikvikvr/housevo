39435d0e9689f2b5b3738e1696aabd63d9f13117
Viktor <ricchiuto.viktor@gmail.com>
2021-07-27 22:52:03 +0200

    feat: add basic layout to accessories picker

--- components/ProductBuilder/AccessoriesPicker/AccessoriesPicker.tsx
+++ components/ProductBuilder/AccessoriesPicker/AccessoriesPicker.tsx
@@ -13,22 +13,19 @@ function AccessoriesPicker({
   toggleAccessory,
 }: Props): JSX.Element {
   return (
-    <div>
-      <h1>acc picker</h1>
-      <ul>
-        {accessories.map((accessory, index) => (
-          <li key={accessory.name}>
-            <div>{accessory.name}</div>
-            <div>{accessory.price}</div>
-            <input
-              type="checkbox"
-              onChange={() => toggleAccessory(index)}
-              checked={selectedAccessories.includes(index)}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className={styles.container}>
+      {accessories.map((accessory, index) => (
+        <li className={styles.accessoryItem} key={accessory.name}>
+          <div>{accessory.name}</div>
+          <div>{accessory.price}</div>
+          <input
+            type="checkbox"
+            onChange={() => toggleAccessory(index)}
+            checked={selectedAccessories.includes(index)}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
 
