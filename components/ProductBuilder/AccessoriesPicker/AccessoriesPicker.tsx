2f6809717245a54db6c1aea80373c9981fa229d0
Viktor <ricchiuto.viktor@gmail.com>
2021-07-28 13:25:15 +0200

    style: improve accessories picker appearance

--- components/ProductBuilder/AccessoriesPicker/AccessoriesPicker.tsx
+++ components/ProductBuilder/AccessoriesPicker/AccessoriesPicker.tsx
@@ -16,9 +16,22 @@ function AccessoriesPicker({
     <ul className={styles.container}>
       {accessories.map((accessory, index) => (
         <li className={styles.accessoryItem} key={accessory.name}>
-          <div>{accessory.name}</div>
-          <div>{accessory.price}</div>
+          <p className={styles.name}>{accessory.name}</p>
+          <span className={styles.price}>${accessory.price}</span>
           <input
+            className={styles.checkbox}
+            type="checkbox"
+            onChange={() => toggleAccessory(index)}
+            checked={selectedAccessories.includes(index)}
+          />
+        </li>
+      ))}
+      {accessories.map((accessory, index) => (
+        <li className={styles.accessoryItem} key={accessory.name}>
+          <p className={styles.name}>{accessory.name}</p>
+          <span className={styles.price}>${accessory.price}</span>
+          <input
+            className={styles.checkbox}
             type="checkbox"
             onChange={() => toggleAccessory(index)}
             checked={selectedAccessories.includes(index)}
