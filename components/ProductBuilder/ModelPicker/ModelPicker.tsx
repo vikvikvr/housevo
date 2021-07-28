83d1cdfa6aef7a7462fb5987deb69a59a7ba429f
Viktor <ricchiuto.viktor@gmail.com>
2021-07-28 18:15:30 +0200

    style: improve radio buttons appearance

--- components/ProductBuilder/ModelPicker/ModelPicker.tsx
+++ components/ProductBuilder/ModelPicker/ModelPicker.tsx
@@ -1,6 +1,7 @@
 import styles from "./ModelPicker.module.scss";
 import Image from "next/image";
 import { CarModel } from "types";
+import { checkIcon } from "assets/images";
 
 interface Props {
   selectedModel: CarModel;
@@ -20,22 +21,24 @@ function ModelPicker({
           className={styles.modelContainer}
           onClick={() => onSelectModel(index)}
           key={model.name}
+          data-checked={selectedModel.name === model.name}
         >
           <h1>{model.name}</h1>
+          {/* TODO: figure out out to replace with <Image /> */}
           <img
             className={styles.modelImage}
             src={model.imageUrl}
             alt="model image"
           />
           <span className={styles.price}>from ${model.basePrice}</span>
-          <input
-            className={styles.radioInput}
-            type="radio"
-            name="model"
-            value={selectedModel.name}
-            checked={selectedModel.name === model.name}
-            readOnly
-          />
+          <div
+            className={styles.radioButton}
+            data-checked={selectedModel.name === model.name}
+          >
+            <span className={styles.iconContainer}>
+              <Image src={checkIcon} alt="check icon" />
+            </span>
+          </div>
         </li>
       ))}
     </ul>
