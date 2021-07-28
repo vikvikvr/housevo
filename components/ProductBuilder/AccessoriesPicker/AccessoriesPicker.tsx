86dc2ca2296c60cdcc80c2d73aca5ede2bfc778d
Viktor <ricchiuto.viktor@gmail.com>
2021-07-28 14:04:28 +0200

    style: improve checkboxes appearance

--- components/ProductBuilder/AccessoriesPicker/AccessoriesPicker.tsx
+++ components/ProductBuilder/AccessoriesPicker/AccessoriesPicker.tsx
@@ -1,5 +1,7 @@
 import styles from "./AccessoriesPicker.module.scss";
 import { CarAccessory } from "types";
+import { checkIcon } from "assets/images";
+import Image from "next/image";
 
 interface Props {
   selectedAccessories: number[];
@@ -14,30 +16,26 @@ function AccessoriesPicker({
 }: Props): JSX.Element {
   return (
     <ul className={styles.container}>
-      {accessories.map((accessory, index) => (
-        <li className={styles.accessoryItem} key={accessory.name}>
-          <p className={styles.name}>{accessory.name}</p>
-          <span className={styles.price}>${accessory.price}</span>
-          <input
-            className={styles.checkbox}
-            type="checkbox"
-            onChange={() => toggleAccessory(index)}
-            checked={selectedAccessories.includes(index)}
-          />
-        </li>
-      ))}
-      {accessories.map((accessory, index) => (
-        <li className={styles.accessoryItem} key={accessory.name}>
-          <p className={styles.name}>{accessory.name}</p>
-          <span className={styles.price}>${accessory.price}</span>
-          <input
-            className={styles.checkbox}
-            type="checkbox"
-            onChange={() => toggleAccessory(index)}
-            checked={selectedAccessories.includes(index)}
-          />
-        </li>
-      ))}
+      {accessories.map((accessory, index) => {
+        const isChecked = selectedAccessories.includes(index);
+        return (
+          <li
+            className={styles.accessoryItem}
+            data-checked={isChecked}
+            onClick={() => toggleAccessory(index)}
+            key={accessory.name}
+          >
+            <p className={styles.name}>{accessory.name}</p>
+            <span className={styles.price}>${accessory.price}</span>
+            {/* TODO: extract checkbox to seaprate component */}
+            <div className={styles.checkbox} data-checked={isChecked}>
+              <span className={styles.checkIcon}>
+                <Image src={checkIcon} alt="check icon" />
+              </span>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
