966a1a0985929625a4af5f7ebce055314bb549da
Viktor <ricchiuto.viktor@gmail.com>
2021-07-28 19:07:45 +0200

    style: make header responsive

--- components/ProductBuilder/WizardHeader/WizardHeader.tsx
+++ components/ProductBuilder/WizardHeader/WizardHeader.tsx
@@ -12,22 +12,30 @@ const headerTitles = ["select model", "select color", "accessories", "summary"];
 function WizardHeader({ step, selectStep }: Props): JSX.Element {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>{headerTitles[step]}</h1>
+      <h1 className={styles.titleMobile}>{headerTitles[step]}</h1>
+      <h1 className={styles.titleDesktop}>product builder</h1>
       <span className={styles.stepCounter}>Step {step + 1} of 4</span>
       <ul className={styles.links}>
-        <li onClick={() => selectStep(0)}>models</li>
-        <li onClick={() => selectStep(1)}>colors</li>
-        <li onClick={() => selectStep(2)}>accessories</li>
-        <li onClick={() => selectStep(3)}>summary</li>
+        <li data-selected={step === 0} onClick={() => selectStep(0)}>
+          models
+        </li>
+        <li data-selected={step === 1} onClick={() => selectStep(1)}>
+          colors
+        </li>
+        <li data-selected={step === 2} onClick={() => selectStep(2)}>
+          accessories
+        </li>
+        <li data-selected={step === 3} onClick={() => selectStep(3)}>
+          summary
+        </li>
       </ul>
-      {step === 0 && (
-        <button className={styles.backButton}>
-          <span className={styles.iconContainer}>
-            <Image src={arrowLightIcon} alt="arrow icon" />
-          </span>
-          <span>Article & Download</span>
-        </button>
-      )}
+      <button className={styles.backButton} data-step={step}>
+        {/* TODO: show icon for desktop */}
+        <span className={styles.iconContainer}>
+          <Image src={arrowLightIcon} alt="arrow icon" />
+        </span>
+        <span>Article & Download</span>
+      </button>
     </div>
   );
 }
